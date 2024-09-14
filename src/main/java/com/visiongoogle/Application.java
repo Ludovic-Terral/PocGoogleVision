235bbb08d22f5487c2a208e5200c06a7646c2d1c
ludovic TERRAL <ludovic.terral@lixtec.fr>
2024-09-14 11:39:04 +0200

    initial version

--- src/main/java/com/visiongoogle/Application.java
+++ src/main/java/com/visiongoogle/Application.java
@@ -1,13 +1,15 @@
 package com.visiongoogle;
 
+import java.io.PrintStream;
+
 public class Application {
 
     public static void main(String[] args) {
 
         System.out.println("Trying the google cloud vision api");
         try {
-            Vision.detectDocumentsGcs("gs://simple-pdf-soruce/pdf-test.pdf","gs://simple-pdf-destination/response/");
-            //Vision.detectText("/Users/mansubh/projects/visiongoogle/simple.jpg", new PrintStream("/Users/mansubh/projects/visiongoogle/result1.txt"));
+//            Vision.detectDocumentsGcs("gs://simple-pdf-soruce/pdf-test.pdf","gs://simple-pdf-destination/response/");
+            Vision.detectText(Application.class.getResource("/pdf-test.pdf").getPath(), new PrintStream("W:\\_dev\\_isen\\PocGoogleVision/result1.txt"));
 
         }catch (Exception e){
             System.out.println("Exception -> "+ e.getMessage());
